ff1ae9417274d1403065d36dfbc9d829d1cca96d
Jirka Mayer <jirkamayer@seznam.cz>
2016-12-28 13:36:23 +0100

    Přepsání týmu na PHP

---
+++ data/tym.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Seznam všech členů týmu
+ */
+return [
+
+    /**
+     * Patrik
+     */
+    [
+        "name" => "Patrik Půlpán",
+        "nickname" => "Paťa",
+        "town" => "Slatiňany",
+        "img" => "patrik.png",
+
+        "likes" => "Skauting, Organizace akcí, divadlo",
+        "meet" => "Všude mě je všude plno a hlavně jsem slyšet přes celé tábořiště :)",
+        "about" => "Jsem skaut už 13 let a pořád mě to baví! Jsem vůdce jedné smečky vlčat, motám se kolem vedení jednoho střediska, podílím se realizaci dalších kurzů :) a ještě pár věcí ... Mimo to jsem student a ještě nevím čím bych chtěl v životě být (ale popelářem už ne!)",
+        "education" => [
+            "ČK Tokaheya (2014)",
+            "ZZA Podlitina (2015)",
+            "VLK Quo Vadis (2015)"
+        ],
+        "take" => "Špunty do uší, dobrou náladu a hlavně peřinky!",
+        "motto" => "Kdo chce jiné zapalovat, musí sám hořet!"
+    ],
+
+    /**
+     * Tereza
+     */
+    [
+        "name" => "Tereza Koberová",
+        "nickname" => "Sňéža",
+        "town" => "Slatiňany",
+        "img" => "tereza.jpg",
+
+        "likes" => "Trempování",
+        "meet" => "49° 53' 18.0901577\" N 15° 46' 10.357132\" E",
+        "about" => "Mé jméno už znáte a studuji medicinální chemii (takže si s sebou raději vemte ochutnávače). Skautuju ve Slatiňanech od té doby, co jsem přišla na světlušácký nábor. Prázdniny ráda trávím v přírodě, baví mě objevovat nová místa - v zimě na běžkách, v létě pěšmo.",
+        "education" => [
+            "ČK Tokaheya (2014)",
+            "ZZA Podlitina (2015)",
+            "VLK Quo Vadis (2015)"
+        ],
+        "take" => "To, co mi při balení cvrnkne o nos.",
+        "motto" => "Žij naplno (skoro) každý den!"
+    ],
+
+    /**
+     * Ondra
+     */
+    [
+        "name" => "Ondřej Procházka",
+        "nickname" => "MR. OP",
+        "town" => "Sobětuchy?",
+        "img" => "ondra.png",
+
+        "likes" => "",
+        "meet" => "",
+        "about" => "",
+        "education" => [
+        ],
+        "take" => "",
+        "motto" => ""
+    ],
+];
