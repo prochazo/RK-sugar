420268f4ee548e46ac76fa421b2166f511040aaa
prochazo <ondra98prochazka@gmail.com>
2016-12-29 11:05:53 +0100

    Doplnění údajů

--- data/tym.php
+++ data/tym.php
@@ -39,9 +39,7 @@ return [
         "meet" => "49° 53' 18.0901577\" N 15° 46' 10.357132\" E",
         "about" => "Mé jméno už znáte a studuji medicinální chemii (takže si s sebou raději vemte ochutnávače). Skautuju ve Slatiňanech od té doby, co jsem přišla na světlušácký nábor. Prázdniny ráda trávím v přírodě, baví mě objevovat nová místa - v zimě na běžkách, v létě pěšmo.",
         "education" => [
-            "ČK Tokaheya (2014)",
-            "ZZA Podlitina (2015)",
-            "VLK Quo Vadis (2015)"
+            "2014 Podlitina", "2015 ČLK Odyssea", "2017 VLK Velká Morava",
         ],
         "take" => "To, co mi při balení cvrnkne o nos.",
         "motto" => "Žij naplno (skoro) každý den!"
@@ -53,7 +51,7 @@ return [
     [
         "name" => "Ondřej Procházka",
         "nickname" => "MR. OP",
-        "town" => "Sobětuchy?",
+        "town" => "Sobětuchy",
         "img" => "ondra.png",
 
         "likes" => "",
@@ -63,5 +61,65 @@ return [
         ],
         "take" => "",
         "motto" => ""
+    ],
+
+    /**
+     * Přemek
+     */
+    [
+        "name" => "Přemysl Janda",
+        "nickname" => "Přemek",
+        "town" => "Slatiňany",
+        "img" => "ondra.png",
+
+        "likes" => "",
+        "meet" => "To se ještě uvidí, ale jisté je že žádnou rozcvičku nevynechám.",
+        "about" => "Druhým rokem studuji vysokou zednickou na ČVUT v Praze. Už nějaký ten pátek skautuju v 1. oddíle Dúnadan ve Slatiňanech. Občas si rád zahraju frisbee, mám radši slané, ale sladkým nikdy nepohrdnu. ",
+        "education" => [
+            "2014 ČK Zvostra",
+            "2016 ZZA Cukříček", 
+        ],
+        "take" => "Nikdy nesmí chybět polštář.Dobrý spánek vždy osladí život.",
+        "motto" => "Vždycky se to nějak udělá."
+    ],
+
+    /**
+     * Šimon
+     */
+    [
+        "name" => "Šimon Kimák",
+        "nickname" => "Krocan",
+        "town" => "Slatiňany",
+        "img" => "ondra.png",
+
+        "likes" => "Skauting, příroda, literatura frisbee, běžky.",
+        "meet" => "na přednáškách a občas zaskočím do kurzovní kuchyně (ikdyž teď už jenom ze zvědavosti)",
+        "about" => "Jsem Šíma a studuji na gymplu, mám moc rád přírodu a taky mě baví chemie. Rád bych se v budoucnu věnovat studiu přírodních věd, či medicíny.
+                    Skautem jsem od první třídy. Už čtyři a půl roku rádcuji vlčata, nyní jsem zástupce vůdce oddílu. 
+                    mezi mé záliby patří fantazi literatura (např: Úžasná Zeměplocha), rád vařím a chodím na dlouhé procházky a nebo jezdím na kole.",
+        "education" => [
+        "2015 ČLK Corda", "2015 ZZA Quo Vadis", "2015 VLK Gong",
+        ],
+        "take" => "dobrou knížku, křesadlo a veselý úsměv",
+        "motto" => "Mám taková dvě životní motta, jedno je takové které mě celkem vystihuje a to druhá je takové krásně hlubokomyslné. 1.Zatopíš, uvaříš, pomůžeš, vysvětlíš.  To druhé motto je citát z mé oblíbené knihy: ,,Rozhodovat můžeme jen o tom, co udělat s časem, který nám byl dán... (Gandalf) Společenstvo prstenu"
+    ],
+
+      /**
+     * Sylva
+     */
+    [
+        "name" => "Sylva Coufalová",
+        "nickname" => "Syslík",
+        "town" => "Kroměříž",
+        "img" => "patrik.png",
+
+        "likes" => "",
+        "meet" => "Na kurzu budu hlavně vařit a usmívat se na vás, možná pomůžu s nějakou přednáškou:)",
+        "about" => "Ahoj, je mi 16 a skautka jsem od svých 8 let. Chodím na gymnázium. Mám ráda skauting, jídlo, internet, facebook, instagram, postel, biologii, zsv, vaření, malování a chození na procházky:)",
+        "education" => [
+        "2015 RK Cukříček",
+        ],
+        "take" => "Určitě si vezmu polštář a rukavice, protože u ohně je někdy zima. Jídlo rozhodně ne, protože bych pak neměla místo na to skvělý, co na nás čeká na kurzu! <3",
+        "motto" => "\"Jez dokud můžeš, protože nikdy nevíš, kdy budeš jíst příště!:)\""
     ],
 ];
